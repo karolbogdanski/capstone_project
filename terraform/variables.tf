dcf8caf532e487c9cc8eb75f484ee8daf0bff84f
testuser681 <piotrdrabicki99@gmail.com>
2021-10-05 14:44:55 +0200

    new variables file

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default     = "us-east-2"
+  description = "AWS region"
+}
+
+variable "user_id" {
+  default     = "113304117666"
+  description = "User ID"
+}
+
+
+variable "bucket_name" {
+    default     = "backup-s3"
+    descirption = "S3 bucket for terraform backup"
+}
