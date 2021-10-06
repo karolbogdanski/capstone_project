0dc635628399b559fcb02118fe967d72943ec1af
karolbogdanski <karol.bogdanski3@gmail.com>
2021-10-06 10:32:58 +0200

    changed helm chart repo

--- terraform/modules/jenkins/jenkins.tf
+++ terraform/modules/jenkins/jenkins.tf
@@ -16,9 +16,9 @@ resource "kubernetes_namespace" "jenkins" {
 
 resource "helm_release" "jenkins" {
   name       = "jenkins"
-  repository = "https://charts.helm.sh/stable"
+  repository = "https://charts.bitnami.com/bitnami"
   chart      = "jenkins"
-  version    = "2.0.1"
+  version    = "8.0.14"
   namespace  = "jenkins"
   //TODO check if it gets right when we wait a little longer
   timeout    = 600
