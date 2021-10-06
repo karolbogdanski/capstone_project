bdd7cb43d633d1b4b5ae130d240d6510ba0611e0
testuser681 <piotrdrabicki99@gmail.com>
2021-10-06 14:15:17 +0200

    remove not needed comments

--- terraform/modules/jenkins/jenkins.tf
+++ terraform/modules/jenkins/jenkins.tf
@@ -1,26 +1,9 @@
-/*
-resource "kubernetes_namespace" "jenkins" {
-  metadata {
-    annotations = {
-      name = "example-annotation"
-    }
-
-    labels = {
-      mylabel = "label-value"
-    }
-
-    name = "terraform-example-namespace"
-  }
-}
-*/
-
 resource "helm_release" "jenkins" {
   name       = "jenkins"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "jenkins"
   version    = "8.0.14"
   namespace  = "jenkins"
-  //TODO check if it gets right when we wait a little longer
   timeout    = 600
   values = [
     "${file("./modules/jenkins/helm-values.yaml")}"
