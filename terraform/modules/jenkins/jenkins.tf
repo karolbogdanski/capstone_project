4f8e5149f91a2ff4107a4b051d5f11ace8cbabe4
testuser681 <piotrdrabicki99@gmail.com>
2021-10-08 10:40:30 +0200

    jenkins with plugins

--- terraform/modules/jenkins/jenkins.tf
+++ terraform/modules/jenkins/jenkins.tf
@@ -1,11 +1,13 @@
 resource "helm_release" "jenkins" {
-  name       = "jenkins"
-  repository = "https://charts.jenkins.io"
-  chart      = "jenkins"
-  version    = "3.6.1"
-  namespace  = "jenkins"
-  timeout    = 300
+  name             = "jenkins"
+  repository       = "https://charts.jenkins.io"
+  chart            = "jenkins"
+  version          = "3.6.1"
+  create_namespace = true
+  namespace        = "jenkins"
+  timeout          = 300
   values = [
     "${file("./modules/jenkins/helm-values.yaml")}"
   ]
+  lint = true
 }
