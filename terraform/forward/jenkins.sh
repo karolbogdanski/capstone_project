000868df92d3dbd74e18d153c548f4062fabfb4d
karolbogdanski <karol.bogdanski3@gmail.com>
2021-10-18 15:49:27 +0200

    fixed minor issues

--- terraform/forward/jenkins.sh
+++ terraform/forward/jenkins.sh
@@ -1,2 +1,2 @@
 export POD_NAME=$(kubectl get pods --namespace jenkins -l "app.kubernetes.io/component=jenkins-master" -l "app.kubernetes.io/instance=jenkins" -o jsonpath="{.items[0].metadata.name}")
-kubectl --namespace jenkins port-forward $POD_NAME 3002:8080
+kubectl --namespace jenkins port-forward $POD_NAME 3000:8080
