83a114411ad815f783139ec41cc7b89f12a10383
testuser681 <piotrdrabicki99@gmail.com>
2021-10-19 11:51:32 +0200

    bootstrap argument

--- terraform/template_file.tf
+++
@@ -1,8 +0,0 @@
-data "template_file" "user_data" {
-    template = "${file("templates/user_data.tpl")}"
-
-    vars = {
-        bootstrap_extra_args = "--enable-docker-bridge true"
-        cluster_name         = local.cluster_name
-    }
-}
