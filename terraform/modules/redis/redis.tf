1729818a3665743a1351ea5edcd40e1e5d91574c
karolbogdanski <karol.bogdanski3@gmail.com>
2021-10-22 14:41:23 +0200

    fixed name of repo

--- terraform/modules/redis/redis.tf
+++ terraform/modules/redis/redis.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "redis" {
   name  = "redis"
-  chart = "bitnami/redis"
+  chart = "redis"
   repository       = "https://charts.bitnami.com/bitnami"
   namespace        = "jenkins"
   timeout          = 300
